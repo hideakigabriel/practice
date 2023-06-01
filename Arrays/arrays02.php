760b3780d1996b76ceeb0b340c29fb981b54d6c3
Gabriel Hideaki <hideaki.gabriel@gmail.com>
2023-06-01 00:22:19 -0300

    Update arrays02.php

---
+++ Arrays/arrays02.php
@@ -0,0 +1,9 @@
+<?php
+      $aluno = array("Curso" => "Tec. em Análise e Desenv. de Sistemas",
+            "Nome" => "Gabriel Hideaki",
+            "RA" => 456.682.2523);
+      echo ("Dados do aluno:" .
+            "<BR><strong>Nome: </strong>". $aluno['Nome'] .
+            "<BR><strong>Curso: </strong>". $aluno['Curso'] .
+            "<BR><strong>RA: </strong>". $aluno['RA']);
+?>
