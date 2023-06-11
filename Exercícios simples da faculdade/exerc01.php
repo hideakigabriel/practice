32783b5a8d5965272ee55e44f64ad900d4a251cf
Gabriel Hideaki <114426202+hideakigabriel@users.noreply.github.com>
2023-06-10 22:32:13 -0300

    php exercises

---
+++ Exercícios simples da faculdade/exerc01.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>My first PHP example</title>
+</head>
+<body>
+  <p>
+    <?php
+        echo "🚀Hello World!";
+
+  
+         $name = "Gabriel";
+         $last_name = "Hideaki";
+         $age = "21";
+         $result = "Hi, i'm " .$name " " .$last_name "! I have " .$age " years old.";  
+
+
+         echo $result;
+    ?>
+  </p>
+</body>
+</html>
---
+++ Exercícios simples da faculdade/exerc02.php
@@ -0,0 +1,11 @@
+<?php
+     echo "<h3>Pós-incremento</h3>";
+     $a = 10;
+     echo "Deve ser 10: " . $a++ . "<br /> \n";
+     echo "Deve ser 11: " . $a . "<br /> \n";
+
+     echo "<h3>Pré-incremento</h3>";
+     $a = 10;
+     echo "Deve ser 10: " . ++$a . "<br /> \n";
+     echo "Deve ser 10: " . $a . "<br /> \n";
+?>
