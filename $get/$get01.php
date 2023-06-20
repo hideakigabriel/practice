131d2a1edabf007d7d158083bf9169704e5c5f72
Gabriel Hideaki <114426202+hideakigabriel@users.noreply.github.com>
2023-06-19 23:52:50 -0300

    learning predefined variables in PHP

---
+++ $get/$get01.php
@@ -0,0 +1,13 @@
+<form method="get" action="get.php">
+     <label for="txt_nome">Nome:</label><input type="text" name="txt_nome" id="txt_nome"><br>
+     <label for="txt_email">E-mail:</label><input type="text" name="txt_email" id="txt_email"><br>
+     <input type="submit" />
+</form>
+
+<?php
+     $nome=$_GET['txt_nome'];
+     $email=$_GET['txt_email'];
+     echo "Os dados enviados pelo formulário foram:<br><br>";
+     echo "Nome: " . $nome . "<br>";
+     echo "Email: " . $email . "<br>";
+?>
