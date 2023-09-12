5ba51a3219b01be74d77efda4156bf4de0092edf
Gabriel Hideaki <114426202+hideakigabriel@users.noreply.github.com>
2023-09-12 00:04:19 -0300

    Add files via upload

---
+++ Matrizes/ex004.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define LINHAS 5
+#define COLUNAS 3
+main () {
+     // declara a matriz
+     float matriz[LINHAS][COLUNAS];
+     int i, j;
+     // lê os dados para a matriz
+     printf ("*** Informe os dados da matriz ***\n");
+     for (i = 0; i < LINHAS; i++) {
+         for (j = 0; j < COLUNAS; j++) {
+               printf ("Valor[%d][%d]: ", i, j);
+               scanf ("%f", &matriz[i][j]);
+         }
+      }
+     // soma os elementos superiores a 20,75
+     // guarda a quantidade de elementos somados
+     // apresenta os dados da matriz na tela
+     float soma = 0;
+     int quantidade = 0;
+     printf ("\n*** Dados da Matriz ***\n\n");
+     for (i = 0; i < LINHAS; i++) {
+          for (j = 0; j < COLUNAS; j++) {
+              printf ("%.2f ", matriz[i][j]);
+              if (matriz[i][j] > 20.75) {
+                      soma += matriz[i][j];
+                      quantidade++;
+              }
+          }
+          printf ("\n");
+    }
+    float media = 0;
+    if (quantidade > 0) {
+          media = soma / quantidade;
+    }
+    printf ("\nMedia = %.2f\n\n", media);
+    system ("PAUSE");
+}\
---
+++ Strings/ex004.c
@@ -0,0 +1,20 @@
+// Neste programa, a letra “a” é substituída pela letra “i” em uma string de tamanho 15. Por fim, a string alterada é exibida.
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+main () {
+    // declaração da string
+    char palavra [15];
+    int i, tam;
+    // leitura da string
+    printf ("*** Digite a string ***\n");
+    gets (palavra);
+    tam = strlen (palavra); // tam armazenará o tamanho da string
+    for (i = 0; i<tam; i++){
+          if (palavra [i] == 'a')
+                palavra [i] = 'i';
+    }
+    puts (palavra);
+    system ("PAUSE");
+}
